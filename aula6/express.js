6b99b3b141fc7d20960975bad36b7b0304715857
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2022-09-13 15:46:00 -0300

    Update express.js

--- aula6/express.js
+++ aula6/express.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 
-
+app.get("/", function(req,res){
+    res.send("Bem vindo");
+});
 app.listen(8081, function(){
     console.log("Servidor rodando");
 });
