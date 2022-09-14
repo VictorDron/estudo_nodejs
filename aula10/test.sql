75ecba93d04d9f66987c9ade129c9514b8c1012f
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2022-09-14 19:10:42 -0300

    Update test.sql

--- aula10/test.sql
+++ aula10/test.sql
@@ -1,3 +1,4 @@
+
 CREATE TABLE usuarios(
     nome VARCHAR(50),
     email VARCHAR(100),
@@ -9,4 +10,10 @@ CREATE TABLE price (
     fechamento FLOAT(10),
     maximo FLOAT(10),
     minimo FLOAT(10)
+);
+
+INSERT INTO usuarios(nome,email,idade) VALUES(
+    'Victor',
+    'Vic@teste',
+     22
 );
