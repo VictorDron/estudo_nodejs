0e01f778b17bab0884b70519d7cb1d5795d4ca7f
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2022-09-16 21:12:06 -0300

    upadate

--- express.js
+++ express.js
@@ -1,5 +1,14 @@
+//Chamada ao Express
 const express = require ("express");
 const app = express();
+//Chamada ao Sequelize para conectar o banco de dados mysql
+const Sequelize = require ('sequelize');
+const sequelize = new Sequelize (/*nome do banco*/'victor',/*Tipo de acesso*/'root',/*Senha*/'153120',{
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+
 
 app.get('/',function(req,res){
 
@@ -7,4 +16,4 @@ app.get('/',function(req,res){
 
 app.listen(8081, function(){
     console.log('Servido ON');
-});
+}); 
