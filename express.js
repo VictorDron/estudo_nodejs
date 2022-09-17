5e4eec2cc7490ec95884af49be4cf9fd711da6c8
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2022-09-16 21:24:17 -0300

    ok

--- express.js
+++ express.js
@@ -1,4 +1,4 @@
-//Chamada ao Express
+//Chamada ao Express, que é responsável por criar  o servidor e suas rotas
 const express = require ("express");
 const app = express();
 //Chamada ao Sequelize para conectar o banco de dados mysql
@@ -6,10 +6,15 @@ const Sequelize = require ('sequelize');
 const sequelize = new Sequelize (/*nome do banco*/'victor',/*Tipo de acesso*/'root',/*Senha*/'153120',{
     host: 'localhost',
     dialect: 'mysql'
-})
+});
+
+// Chamada ao handlebars, que é responsável por conectar o front ao back
+const handlebars = require('express-handlebars');
+//Configuração do HandleBars
+app.engine('handlebars,' handlebars.engine({defaultLayout}));
 
 
-
+//Criação de rota com o método GET
 app.get('/',function(req,res){
 
 });
