fa3a25bb8fe3131303ce0599c4b546369693664f
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2022-09-18 17:05:31 -0300

    OK

--- app.js
+++ app.js
@@ -20,33 +20,39 @@ app.set(bodyParser.json());
 app.engine('handlebars', handlebars.engine({defaultLayout:'main'}));
 app.set('view engine','handlebars');
 
+//Rotas
 
-//Criação de rota para renderizar o HTML.
-app.get('/form',function(req,res){
-    res.render('formulario');
+    //Criação de rota para renderizar o HTML.
+    app.get('/form',function(req,res){
+        res.render('formulario');
 
-});
+    });
 
-//Rota para recebimento dos dados do front
-app.post('/add', function(req,res){
+    //Rota para recebimento dos dados do front
+    app.post('/add', function(req,res){
 
-    //Recebimento e armazenamento
-    let nome = req.body.titulo; // Localizando e armazenando na variavel o que virá do campo nome
-    let cont = req.body.conteudo;
-    
-    //Chamda da função para registrar os dados na table do db postapp
-    Post.create({
-
-        titulo: nome,
-        conteudo: cont
+        //Recebimento e armazenamento
+        let nome = req.body.titulo; // Localizando e armazenando na variavel o que virá do campo nome
+        let cont = req.body.conteudo;
         
-    }).then(function(){
-        res.send('post criado com sucesso');
-    }).catch(function(){
-        res.send('Erro ao criar post');
+        //Chamda da função para registrar os dados na table do db postapp
+        Post.create({
+
+            titulo: nome,
+            conteudo: cont
+
+        })./*tESTE SE O REGISTRO DEU CERTO*/then(function(){
+            res.redirect('/')
+
+        }).catch(function(){
+            res.send('Erro ao criar post');
+        })
+        
     })
-    
-})
+
+    app.get('/', function(req,res){
+        res.render('home');
+    })
 
 app.listen(8081, function(){
     console.log('Servido ON');
---
+++ views/home.handlebars
@@ -0,0 +1,1 @@
+<h1>BEM VINDO</h1>
